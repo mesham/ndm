c96371f1e0f7553cc5ce56032b1dcd2f46472c21
Nick <nick.brown@ed.ac.uk>
2016-05-04 10:54:56 +0100

    Additional example groups with new disjoint call

--- examples/src/example_groups.c
+++ examples/src/example_groups.c
@@ -10,16 +10,18 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  int provided, my_rank, shrunk_rank, extracted_rank;
+  int provided, my_rank, shrunk_rank, extracted_rank, disjoint_rank;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
   ndmInit();
   ndmGroupRank(NDM_GLOBAL_GROUP, &my_rank);
-  NDM_Group shrunkGroup, extractedGroup;
+  NDM_Group shrunkGroup, extractedGroup, disjointGroup;
   ndmGroupCreateWithStride(&shrunkGroup, NDM_GLOBAL_GROUP, 11, 1);
   ndmGroupRank(shrunkGroup, &shrunk_rank);
   ndmGroupExtractChunkContainingRank(&extractedGroup, NDM_GLOBAL_GROUP, 12, my_rank);
   ndmGroupRank(extractedGroup, &extracted_rank);
-  printf("%d and %d and %d\n", my_rank, shrunk_rank, extracted_rank);
+  ndmGroupCreateDisjoint(&disjointGroup, NDM_GLOBAL_GROUP, shrunkGroup);
+  ndmGroupRank(disjointGroup, &disjoint_rank);
+  printf("%d and %d and %d and %d\n", my_rank, shrunk_rank, extracted_rank, disjoint_rank);
   ndmFinalise();
   MPI_Finalize();
   return 0;
