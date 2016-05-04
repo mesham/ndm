884ccfb98a4cf5c1f12fb5b71271a238379e1d96
Nick <nick.brown@ed.ac.uk>
2016-05-04 13:33:30 +0100

    Reduce example uses additive calls

--- examples/src/example_reduce.c
+++ examples/src/example_reduce.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 
 void recvFunction(void*, NDM_Metadata);
+void additiveRecvFunction(void*, NDM_Metadata);
 
 int main(int argc, char* argv[]) {
   int provided;
@@ -21,6 +22,9 @@ int main(int argc, char* argv[]) {
   ndmReduce(&data, 1, NDM_INT, NDM_SUM, recvFunction, 0, NDM_GLOBAL_GROUP, "a");
   ndmGroupRank(NDM_GLOBAL_GROUP, &data);
   ndmAllReduce(&data, 1, NDM_INT, NDM_MAX, recvFunction, NDM_GLOBAL_GROUP, "maxrank");
+  data = 5;
+  ndmReduceAdditive(&data, 1, NDM_INT, 12, 1, 0, NDM_SUM, additiveRecvFunction, 0, NDM_GLOBAL_GROUP, "additive");
+  ndmReduceAdditive(&data, 1, NDM_INT, 12, 1, 0, NDM_SUM, additiveRecvFunction, 0, NDM_GLOBAL_GROUP, "additive");
   ndmFinalise();
   MPI_Finalize();
   return 0;
@@ -29,3 +33,5 @@ int main(int argc, char* argv[]) {
 void recvFunction(void* buffer, NDM_Metadata metaData) {
   printf("Got reduction data '%d' with uuid %s on pid %d\n", *((int*)buffer), metaData.unique_id, metaData.my_rank);
 }
+
+void additiveRecvFunction(void* buffer, NDM_Metadata metaData) { printf("Got additive data '%d'\n", *((int*)buffer)); }
